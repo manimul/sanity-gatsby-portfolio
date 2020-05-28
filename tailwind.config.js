9f8b892a7a3465746de588708f810c0279986013
Mark Mulvaney <mark@bambwa.com>
2020-05-28 11:34:42 +0200

    tailwind issue, removed purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-     
-    ]
-  },
+
 
   theme: {
     colors:{
