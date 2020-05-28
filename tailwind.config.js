43b7e8e39b90dd560ab0d8f50ef6de4f2a1336d4
Mark Mulvaney <mark@bambwa.com>
2020-05-28 11:27:12 +0200

    removed purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
     content: [
-      'components/**/*.js',
-      'layouts/**/*.js',
-      'pages/**/*.js',
-      'plugins/**/*.js',
-      'gatsby.config.js'
+     
     ]
   },
 
